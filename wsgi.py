43ac05783939290e98230fe4fde8006d797dd5e0
LoneLovewolf <secretjoy024@gmail.com>
2025-10-29 17:27:04 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,5 @@
 import os
-import sys
-
-# Add your project root directory to Python path
-path = '/home/LoneLovewolf'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-# Point Django to your settings file
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-# Load the Django WSGI application
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 application = get_wsgi_application()
